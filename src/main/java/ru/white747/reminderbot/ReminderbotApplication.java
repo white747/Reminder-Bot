02890d00f080bfc8ad8fdfe6f0213bd1fb8e43db
Mikhail_Pigolkin <pigolkin.mikhail@yandex.ru>
2024-10-26 18:26:54 +0300

    шаблон проекта

---
+++ src/main/java/ru/white747/reminderbot/ReminderbotApplication.java
@@ -0,0 +1,13 @@
+package ru.white747.reminderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/white747/reminderbot/ReminderbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.white747.reminderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
